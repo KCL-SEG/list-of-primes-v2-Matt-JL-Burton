aefdcc30340d05e816ea1b8b89e12bae2f0bbf54
Matt-JL-Burton <mslburton24@yahoo.com>
2023-09-30 19:19:33 +0100

    Write code that passes tests

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
     list = []
+    i = 2
+    while (len(list) < number_of_primes):
+        prime = True
+        for j in range(len(list)):
+            if i%list[j] == 0:
+                prime=False
+        if prime:
+            list.append(i)
+        i = i + 1         
     return list
+
+
+primes(20)
